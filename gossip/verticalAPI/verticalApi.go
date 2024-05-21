9d3b46b81e8950b4f34a542504e1f371dc543fab
Lukas Heindl <luk.heindl@tum.de>
2024-05-21 21:19:05 +0200

    draft base logic implementation of vertical api

--- gossip/verticalAPI/verticalApi.go
+++ gossip/verticalAPI/verticalApi.go
@@ -2,8 +2,12 @@ package verticalapi
 
 import (
 	"context"
+	"fmt"
+	vertTypes "gossip/verticalAPI/types"
+	"io"
 	"log/slog"
 	"net"
+	"slices"
 )
 
 // This struct represents the vertical api and is the main interface to/from
@@ -56,7 +60,175 @@ func NewVerticalApi(log *slog.Logger, vertToMainChans VertToMainChans) *Vertical
 // This function spawns a new goroutine accepting new connections and
 // terminates afterwards.
 func (v *VerticalApi) Listen(addr string) (err error) {
-	return
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("listen to port for vertical API: %w", err)
+	}
+
+	// accept all connections on this port
+	go func() {
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				// check if shall terminate
+				select {
+				case <-v.ctx.Done():
+					break
+				default:
+				}
+				slog.Error("Accept for vertical API failed", "err", err)
+				continue
+			}
+
+			v.conns[conn] = struct{}{}
+
+			mainToVert := make(chan MainToVertNotification)
+			regMod := RegisteredModule{
+				MainToVert: mainToVert,
+			}
+
+			go v.handleConnection(conn, regMod)
+			go v.writeToConnection(conn, mainToVert)
+		}
+	}()
+	return nil
+}
+
+// Handle an incoming connection -- read
+//
+// Parses the message header and body. Then sends the message via the
+// respective channel to the main package.
+func (v *VerticalApi) handleConnection(conn net.Conn, regMod RegisteredModule) {
+	var err error
+	var nRead int
+
+	// if this read routine terminates, make sure the connection is cleaned up
+	// properly
+	// avoid double Close when the vertical api is being closed
+	defer delete(v.conns, conn)
+	// close the main > vert channel to signal the connection is closed
+	// also this causes the write routine to terminate
+	defer close(regMod.MainToVert)
+	// close the connection
+	defer conn.Close()
+
+	var msgHdr vertTypes.MessageHeader
+	buf := make([]byte, msgHdr.CalcSize())
+
+	for {
+		// set the size of the slice to make sure not to read too much
+		buf = buf[0:msgHdr.CalcSize()]
+		// read the message header
+		nRead, err = io.ReadFull(conn, buf)
+		if err != nil {
+			// check if shall terminate
+			select {
+			case <-v.ctx.Done():
+				return
+			default:
+			}
+			slog.Error("Read on vertical API failed", "err", err)
+			continue
+		}
+		// it is save that the complete buffer is completely populated at this point
+
+		// parse the message header
+		_, err = msgHdr.Unmarshal(buf)
+		if err != nil {
+			slog.Warn("Invalid header read", "err", err)
+			continue
+		}
+
+		// allocate space for the message body
+		buf = slices.Grow(buf, int(msgHdr.Size)-nRead)
+
+		// read the message body
+		_, err = io.ReadFull(conn, buf[nRead:])
+		if err != nil {
+			// check if shall terminate
+			select {
+			case <-v.ctx.Done():
+				return
+			default:
+			}
+			slog.Error("Read on vertical API failed", "err", err)
+			continue
+		}
+		// it is save that the complete buffer is completely populated at this point
+
+		switch msgHdr.Type {
+
+		case vertTypes.GossipAnnounceType:
+			var ga vertTypes.GossipAnnounce
+			_, err = ga.Unmarshal(buf)
+			if err != nil {
+				slog.Warn("Invalid GossipAnnounce read", "err", err)
+				continue
+			} else {
+				v.vertToMainChans.Anounce <- VertToMainAnnounce{
+					Data: ga,
+				}
+			}
+
+		case vertTypes.GossipNotifyType:
+			var gn vertTypes.GossipNotify
+			_, err = gn.Unmarshal(buf)
+			if err != nil {
+				slog.Warn("Invalid GossipNotify read", "err", err)
+				continue
+			} else {
+				v.vertToMainChans.Register <- VertToMainRegister{
+					Data:   gn,
+					Module: &regMod,
+				}
+			}
+
+		case vertTypes.GossipValidationType:
+			var gv vertTypes.GossipValidation
+			_, err = gv.Unmarshal(buf)
+			if err != nil {
+				slog.Warn("Invalid GossipValidation read", "err", err)
+				continue
+			} else {
+				v.vertToMainChans.Validation <- VertToMainValidation{
+					Data: gv,
+				}
+			}
+
+		default:
+			slog.Warn("vertical API received an unexpected message type", "type", msgHdr.Type)
+		}
+	}
+}
+
+// Write messages to the connection
+//
+// Writes all messages sent to he mainToVert channel to the connection
+func (v *VerticalApi) writeToConnection(conn net.Conn, mainToVert <-chan MainToVertNotification) {
+	var err error
+	var nWritten int
+	buf := make([]byte, 0, 4096)
+
+	for msg := range mainToVert {
+		buf, err = msg.Data.Marshal(buf)
+		if err != nil {
+			slog.Warn("Failed to marshal GossipNotification", "err", err)
+			continue
+		}
+
+		nWritten, err = conn.Write(buf)
+		if err != nil {
+			// check if shall terminate
+			select {
+			case <-v.ctx.Done():
+				return
+			default:
+			}
+			slog.Warn("Failed to send GossipNotification", "err", err)
+			continue
+		}
+		_ = nWritten
+	}
 }
 
 // Close the vertical api
@@ -64,5 +236,17 @@ func (v *VerticalApi) Listen(addr string) (err error) {
 // Always tries to close the listener and all the connections. If multiple
 // fail, this function only returns the last error.
 func (v *VerticalApi) Close() (err error) {
-	return
+	// signal to listener and connection goroutines that they should terminate
+	v.cancel()
+	// interrupt accept of the listener routine
+	if e := v.ln.Close(); e != nil {
+		err = e
+	}
+	for c := range v.conns {
+		// interrupt read of the connection routine
+		if e := c.Close(); e != nil {
+			err = e
+		}
+	}
+	return err
 }
