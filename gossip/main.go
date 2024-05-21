d9206e86d24db2cba644a33bf15adb5f716b50f8
Lukas Heindl <luk.heindl@tum.de>
2024-05-21 20:58:54 +0200

    restructure vertical API and push the adjusted interface we agreed on

--- gossip/main.go
+++ gossip/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	verticalapi "gossip/verticalAPI"
 	"log/slog"
 	"os"
 	"time"
@@ -21,6 +22,14 @@ func main() {
 	// var err error
 	// set up logging
 	slog.SetDefault(logInit())
+
+	vertToMain := verticalapi.VertToMainChans{
+		Register:   make(chan verticalapi.VertToMainRegister),
+		Anounce:    make(chan verticalapi.VertToMainAnnounce),
+		Validation: make(chan verticalapi.VertToMainValidation),
+	}
+	va := verticalapi.NewVerticalApi(slog.With("module", "vertAPI"), vertToMain)
+	_ = va
 
 	// just skip the ini parsing etc for now and only start/listen on the vertical api using constants as address
 	// then later if you want to maybe extract those from the ini config (at a fixed path to skip the argument parsing stuff for now)
