005d9ad8eff61b74155d30cdc29ed96b1fd41202
Fabio Gaiba <faga2703@gmail.com>
2024-05-28 22:15:29 +0200

    Switched from sync Map to traditional Map and Mutex to ensure thread safety during writes

--- gossip/data.go
+++ gossip/data.go
@@ -9,29 +9,28 @@ import (
 // This struct is a dummy channel for specifying MainToVert channels where I can instruct the verticalapi module to send a Gossip Notification message
 
 type notifyMap struct {
-	data sync.Map
+	data map[vertTypes.GossipType][]*verticalapi.RegisteredModule
+	mu   sync.Mutex
 }
 
 func NewNotifyMap() *notifyMap {
 	return &notifyMap{
-		data: sync.Map{},
+		data: make(map[vertTypes.GossipType]([]*verticalapi.RegisteredModule)),
 	}
 }
 
 func (nm *notifyMap) Load(gossip_type vertTypes.GossipType) []*verticalapi.RegisteredModule {
-	res, _ := nm.data.Load(gossip_type)
-	if res == nil {
-		return []*verticalapi.RegisteredModule{}
-	}
-	result, ok := res.([]*verticalapi.RegisteredModule)
-	if !ok {
-		panic("Tried to store in the NotifyMap a value which is not of type GossipType")
-	}
-	return result
+	nm.mu.Lock()
+	defer nm.mu.Unlock()
+	res := nm.data[gossip_type]
+	// if res == nil {
+	// 	return []*verticalapi.RegisteredModule{}
+	// }
+	return res
 }
 
 func (nm *notifyMap) AddChannelToType(gossip_type vertTypes.GossipType, new_channel *verticalapi.RegisteredModule) {
-	current_channels := nm.Load(gossip_type)
+	current_channels := nm.data[gossip_type]
 	new_channels := append(current_channels, new_channel)
-	nm.data.Store(gossip_type, new_channels)
+	nm.data[gossip_type] = new_channels
 }
