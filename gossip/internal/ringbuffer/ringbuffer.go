049bb8398925c42b5a94663c5b090a683b11dff8
Fabio Gaiba <faga2703@gmail.com>
2024-09-08 11:33:13 +0200

    Adding license

--- gossip/internal/ringbuffer/ringbuffer.go
+++ gossip/internal/ringbuffer/ringbuffer.go
@@ -1,3 +1,21 @@
+/*
+* gossip
+* Copyright (C) 2024 Fabio Gaiba and Lukas Heindl
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // Package ringbuffer implements a ringbuffer. It will have a fixed size and if
 // the capacity is exceeded, the oldest value will be overwritten.
 package ringbuffer
